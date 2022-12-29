c6695b4c816334dca91e2cc79a3bea017334636b
ElkinAndrey <andreyridbir@gmail.com>
2022-12-29 16:15:31 +0500

    Создал класс для работы с фальшивой базой данных ресторана

---
+++ RestaurantOrdersAPI/RestaurantOrdersAPI/Models/FakeRestaurantRepository.cs
@@ -0,0 +1,31 @@
+﻿using RestaurantOrdersAPI.Data;
+
+namespace RestaurantOrdersAPI.Models
+{
+    public class FakeRestaurantRepository : IRestaurantRepository
+    {
+        public List<Order> Orders => throw new NotImplementedException();
+
+        public List<Product> Product => throw new NotImplementedException();
+
+        public void AddOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveOrder(int orderNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
