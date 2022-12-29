105fe7e1f2f1a334598ae6319f77595dbdf35a7d
ElkinAndrey <andreyridbir@gmail.com>
2022-12-29 16:11:56 +0500

    Создал интерфейс для работы с базой данных ресторана

---
+++ RestaurantOrdersAPI/RestaurantOrdersAPI/Models/IRestaurantRepository.cs
@@ -0,0 +1,45 @@
+﻿using RestaurantOrdersAPI.Data;
+using System.Security.Principal;
+
+namespace RestaurantOrdersAPI.Models
+{
+    /// <summary>
+    /// Интерфейс для доступа к базе данных ресторана
+    /// </summary>
+    public interface IRestaurantRepository
+    {
+        /// <summary>
+        /// Получить список заказов
+        /// </summary>
+        public List<Order> Orders { get; }
+
+        /// <summary>
+        /// Получить список товаров
+        /// </summary>
+        public List<Product> Product { get; }
+
+        /// <summary>
+        /// Добавить заказ
+        /// </summary>
+        /// <param name="order">Заказ</param>
+        public void AddOrder(Order order);
+
+        /// <summary>
+        /// Удалить заказ
+        /// </summary>
+        /// <param name="orderNumber">Номер удаляемого заказа</param>
+        public void RemoveOrder(int orderNumber);
+        
+        /// <summary>
+        /// Изменить заказ
+        /// </summary>
+        /// <param name="order">Изменяемый заказ</param>
+        public void ChangeOrder(Order order);
+
+        /// <summary>
+        /// Получить заказ
+        /// </summary>
+        public Order GetOrder();
+
+    }
+}
