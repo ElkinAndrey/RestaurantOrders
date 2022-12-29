8036ea25c80e557c52d5221412f08621c303fc4a
ElkinAndrey <andreyridbir@gmail.com>
2022-12-29 15:23:21 +0500

    Создание сущьности 'Заказ'

---
+++ RestaurantOrdersAPI/RestaurantOrdersAPI/Data/Order.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantOrdersAPI.Data
+{
+    /// <summary>
+    /// Заказ
+    /// </summary>
+    public class Order
+    {
+        /// <summary>
+        /// Номер заказа, однозначный ключ
+        /// </summary>
+        [Key]
+        public int OrderNumber { get; set; }
+
+        /// <summary>
+        /// Полная стоимость заказа
+        /// </summary>
+        public decimal TotalPrice { get; set; }
+
+        /// <summary>
+        /// Способ оплаты
+        /// </summary>
+        public string? PaymentMethod { get; set; }
+
+        /// <summary>
+        /// Список продуктов
+        /// </summary>
+        public List<Product> Products { get; set; }
+    }
+}
