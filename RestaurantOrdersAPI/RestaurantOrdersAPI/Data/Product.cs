ec78fb9b5075db8cc046f7bfc60070ebeedeb6fb
ElkinAndrey <andreyridbir@gmail.com>
2022-12-29 15:06:21 +0500

    Создание сущьности 'Товар'

---
+++ RestaurantOrdersAPI/RestaurantOrdersAPI/Data/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantOrdersAPI.Data
+{
+    /// <summary>
+    /// Товар
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Id, однозначный ключ
+        /// </summary>
+        [Key]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Название товара
+        /// </summary>
+        public string? ProductName { get; set; }
+
+        /// <summary>
+        /// Цена товара
+        /// </summary>
+        public decimal ProductPrice { get; set; }
+
+    }
+}
