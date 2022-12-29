5ea404ea8e2fdaf4f5d2057c7486441ed327d004
ElkinAndrey <andreyridbir@gmail.com>
2022-12-29 17:11:56 +0500

    Добавил метод для изменения существующего заказа в фальшивой базе данных

--- RestaurantOrdersAPI/RestaurantOrdersAPI/Models/FakeRestaurantRepository.cs
+++ RestaurantOrdersAPI/RestaurantOrdersAPI/Models/FakeRestaurantRepository.cs
@@ -23,11 +23,23 @@ namespace RestaurantOrdersAPI.Models
         public void AddOrder(Order order)
         {
             FakeDataBase.Orders.Add(order);
+            FakeDataBase.ProductsDetails.AddRange(order.Products);
         }
 
         public void ChangeOrder(Order order)
         {
-            throw new NotImplementedException();
+            Order? oldOrder = FakeDataBase.Orders.FirstOrDefault(o => o.OrderNumber == order.OrderNumber);
+
+            if (oldOrder == null)
+                throw new Exception("Заказ не найден");
+
+            oldOrder.PaymentMethod = order.PaymentMethod;
+            oldOrder.TotalPrice = order.TotalPrice;
+
+            foreach (var item in oldOrder.Products) // Удаление старых товаров
+                FakeDataBase.ProductsDetails.RemoveAll(p => p.ProductDetailsId == item.ProductDetailsId);
+            FakeDataBase.ProductsDetails.AddRange(order.Products); // Добавление новых товаров
+            oldOrder.Products = order.Products;
         }
 
         public Order GetOrder()
