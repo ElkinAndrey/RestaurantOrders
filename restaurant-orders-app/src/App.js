e1ad781e8b7fc7083ed06c579e7bd717fcc904f8
ElkinAndrey <andreyridbir@gmail.com>
2023-01-03 01:46:04 +0500

    Добавление логотипа в на сайте

--- restaurant-orders-app/src/App.js
+++ restaurant-orders-app/src/App.js
@@ -1,35 +1,11 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import Service from "./API";
-import { useFetching } from "./hooks/useFetching";
+import "./App.css";
 
 function App() {
-  const [helloWorld, setHelloWorld] = useState([]); // Массив с информацией
-
-  const [fetchHelloWorld, isHelloWorldLoading, helloWorldError] = useFetching(
-    async () => {
-      const response = await Service.getHelloWorld();
-      setHelloWorld(response.data);
-    }
-  );
-
-  useEffect(() => {
-    fetchHelloWorld();
-  }, []);
-
   return (
-    <div>      
-      {helloWorldError && <div>Произошла ошибка ${helloWorldError}</div>} {/*При Возникновении ошибки (например, если не найдет url сервера)*/}
-      {isHelloWorldLoading ? (
-        <div>Загрузка...</div> // Пока данные получаются с сервера
-      ) : (
-        <div>
-          {/* Данные */}
-          {helloWorld.map((helloWorld) => (
-            <div key={helloWorld}>{helloWorld}</div>
-          ))}
-        </div>
-      )}
+    <div>
+      <div className="logo">Заказы в ресторане</div>
     </div>
   );
 }
