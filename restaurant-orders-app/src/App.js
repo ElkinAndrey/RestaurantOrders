be33ab1e5c0c415b68cc7c10ae89a60a5d791f2b
ElkinAndrey <andreyridbir@gmail.com>
2023-01-03 01:49:19 +0500

    Добавление радио кнопок в приложение

--- restaurant-orders-app/src/App.js
+++ restaurant-orders-app/src/App.js
@@ -1,11 +1,27 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
+import RadioButton from "./components/UI/RadioButton/RadioButton";
 
 function App() {
+  let [checked, setChecked] = useState("CreateOrder"); // id выбранной radio button
+  
   return (
     <div>
       <div className="logo">Заказы в ресторане</div>
+      <RadioButton
+        value={checked}
+        onChange={setChecked}
+        buttons={[
+          { name: "Создать заказ", value: "CreateOrder" },
+          { name: "Заказы", value: "Orders" },
+        ]}
+        margin="0px 20px 20px"
+        position="center"
+        padding="10px 30px"
+        style={{ margin: "0px 0px 10px 0px" }}
+      />
+      <div>{checked}</div>
     </div>
   );
 }
