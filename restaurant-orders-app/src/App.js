472cf635682dc2f98e6c93c1daa1cc162350697c
ElkinAndrey <andreyridbir@gmail.com>
2023-01-03 11:49:33 +0500

    Вынес шапку в отдельный компонент

--- restaurant-orders-app/src/App.js
+++ restaurant-orders-app/src/App.js
@@ -1,27 +1,11 @@
 import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom/client";
 import "./App.css";
-import RadioButton from "./components/UI/RadioButton/RadioButton";
+import Header from "./components/Header";
 
 function App() {
-  let [checked, setChecked] = useState("CreateOrder"); // id выбранной radio button
-  
   return (
     <div>
-      <div className="logo">Заказы в ресторане</div>
-      <RadioButton
-        value={checked}
-        onChange={setChecked}
-        buttons={[
-          { name: "Создать заказ", value: "CreateOrder" },
-          { name: "Заказы", value: "Orders" },
-        ]}
-        margin="0px 20px 20px"
-        position="center"
-        padding="10px 30px"
-        style={{ margin: "0px 0px 10px 0px" }}
-      />
-      <div>{checked}</div>
+      <Header/>
     </div>
   );
 }
