7bfa5f5713c216785da6dbdcad43288786188db7
ElkinAndrey <andreyridbir@gmail.com>
2023-01-04 17:27:15 +0500

    Добавление кнопок для создания и очистки нового заказа

--- restaurant-orders-app/src/components/CreateOrder/CollectedOrder/CollectedOrder.jsx
+++ restaurant-orders-app/src/components/CreateOrder/CollectedOrder/CollectedOrder.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Button from "../../UI/Button/Button";
 import classes from "./CollectedOrder.module.css";
 
 const CollectedOrder = () => {
   return (
     <div className={classes.body}>
-      <div className={classes.buttons}></div>
+      <div style={{marginBottom: "5px"}}>
+        <Button style={{marginRight: "22px"}}>Создать</Button>
+        <Button>Очистить</Button>
+      </div>
       <div className={classes.products}>
         <div>
           <div className={classes.product}>1</div>
