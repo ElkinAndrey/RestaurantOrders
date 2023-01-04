72d56fcad459561471b53d91fe0304d8edd11381
ElkinAndrey <andreyridbir@gmail.com>
2023-01-05 00:29:49 +0500

    Убрал подчеркивание ошибок в строке поиска

--- restaurant-orders-app/src/components/UI/InputSearch/InputSearch.jsx
+++ restaurant-orders-app/src/components/UI/InputSearch/InputSearch.jsx
@@ -44,6 +44,7 @@ const InputSearch = ({
         style={{ background: background, ...style }}
         {...props}
         readOnly={readOnly}
+        spellcheck="false"
       />
       <div id={`${id}-myInput_icon`} className={classes.icon}>
         <img
