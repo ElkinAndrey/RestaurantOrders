bea793e00dae7c647004d0fa193be686b8fdb7d7
ElkinAndrey <andreyridbir@gmail.com>
2023-01-04 13:07:54 +0500

    Создал компоненты с продуктами и создаваемым заказом

---
+++ restaurant-orders-app/src/components/CreateOrder/CollectedOrder/CollectedOrder.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import classes from "./CollectedOrder.module.css"
+
+const CollectedOrder = () => {
+  return (
+    <div className={classes.body}>
+        <div className={classes.buttons}></div>
+        <div className={classes.products}>
+        <div>
+            <div className={classes.product}>1</div>
+            <div className={classes.product}>2</div>
+            <div className={classes.product}>3</div>
+            <div className={classes.product}>4</div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default CollectedOrder
--- restaurant-orders-app/src/pages/CreateOrder.jsx
+++ restaurant-orders-app/src/pages/CreateOrder.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import CollectedOrder from "../components/CreateOrder/CollectedOrder/CollectedOrder";
 import OrderCharacteristics from "../components/CreateOrder/OrderCharacteristics/OrderCharacteristics";
+import ProductList from "../components/CreateOrder/ProductList/ProductList";
 
 const CreateOrder = () => {
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <div style={{background: "red", display: "inline-block"}}>
         <OrderCharacteristics />
+        <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
+          <ProductList/>
+          <CollectedOrder/>
+        </div>
       </div>
     </div>
   );
