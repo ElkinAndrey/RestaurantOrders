93b6077eb6218f0a366db703d5f47d1a9a5f4c2d
ElkinAndrey <andreyridbir@gmail.com>
2023-01-04 20:45:18 +0500

    Добавление счетчика

---
+++ restaurant-orders-app/src/components/UI/Counter/Counter.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef } from "react";
+import classes from "./Counter.module.css";
+
+const Counter = ({ id, value, onChange, ...props }) => {
+  const validate = (v) => {
+    let num = Number(v);
+
+    if (num === "NaN") {
+      return;
+    }
+    if (!Number.isInteger(num)) {
+      return;
+    }
+    if (num <= 0) {
+      onChange(0);
+      return;
+    }
+    if (num > 0 && num < 1000) {
+      onChange(num);
+      return;
+    }
+  };
+
+  return (
+    <div {...props}>
+      <div style={{ display: "flex" }}>
+        <div
+          className={classes.body}
+          style={{ outline: value === 0 ? "1px solid #df0000" : "0px" }}
+        >
+          <button onClick={() => validate(value === 1 ? value : value - 1)}>
+            -
+          </button>
+          <input value={value} onChange={(e) => validate(e.target.value)} />
+          <button onClick={() => validate(value + 1)}>+</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
